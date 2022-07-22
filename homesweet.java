449556e2122bf59fbe8065c748665b9069352b01
Minesh Valand <mpvaland21395@gmail.com>
2022-07-22 20:13:20 +0100

    myplan

--- homesweet.java
+++ homesweet.java
@@ -1,10 +1,14 @@
 public class homesweet {
     public static void main(String[] args) {
-        byte a=30;
-        byte b=15;
-        System.out.println(a+b);//45
-        System.out.println(a-b);//15
-        System.out.println(a*b);//450
-        System.out.println(a/b);//2
+        byte a=60;
+        byte b=10;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a%b);
+        System.out.println(b+a);
+        System.out.println(b-a);
+        System.out.println(b/a);
+        System.out.println(a/b);
     }
 }
